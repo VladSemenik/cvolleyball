ad5148411e26c3f36586da4ba65fe881dd120e97
Владислав Семеник <vlad.semenik@gmail.com>
2019-08-01 10:31:02 +0300

    add fysics aim

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -8,27 +8,51 @@ class Game extends React.Component {
     this.state = {
       data: "Game page",
       ball: {
-        radius: 3,
-        x: 100,
+        radius: 4,
+        x: 30,
         y: 100,
+        dx: 0,
+        dy: 0,
+        gravity: 0.00085,
+        drag: 0.8,
       },
       player: {
+        name: '',
+        color: 'red',
         radius: 6,
         x: 40,
         y: 40,
-        dx: 3,
-        dy: -2,
-        gravity: 3.85,
+        dx: 0,
+        dy: 0,
+        gravity: 0.0085,
         drag: 0.8,
         Right: false,
         Left: false,
         Up: false,
+        Kick: false,
+      },
+      enemy: {
+        name: '',
+        radius: 6,
+        x: 40,
+        y: 40,
+        dx: 0,
+        dy: 0,
+        gravity: 0.0085,
+        drag: 0.8,
+        Right: false,
+        Left: false,
+        Up: false,
+      },
+      aim: {
+        x: 0,
+        y: 0,
       }
     };
   }
 
-  async playerMove () {
-    const player = await Object.assign({}, this.state.player);
+  async playerMove (playerSide = "player") {
+    const player = await Object.assign({}, this.state[playerSide]);
     player.dy -= player.gravity;
     player.y += player.dy;
 
@@ -36,24 +60,74 @@ class Game extends React.Component {
       player.dx = 0;
       player.x = player.radius;
     }
-    if(player.x >= 100 - player.radius) {
+    if(player.x >= 100-player.radius) {
       player.dx = 0;
-      player.x = 100 - player.radius;
+      player.x = 100-player.radius;
     }
-    if(player.y < player.radius) {
-      player.y = player.radius;
+    if(player.y < player.radius*2) {
+      player.y = player.radius*2;
       player.dy = 0;
     }
 
-    if (player.Right) player.x += 5;
-    if (player.Left) player.x -= 5;
-    if (player.Up && player.y === player.radius) {
-      player.dy += 20;
+    if (player.y === player.radius*2) {
+      if (player.Right) player.x += 0.5;
+      if (player.Left) player.x -= 0.5;
+    } else {
+      if (player.Right) player.x += 0.2;
+      if (player.Left) player.x -= 0.2;
+    }
+
+    if (player.Up && player.y === player.radius*2) {
+      player.dy += 1;
       player.Up = false;
     }
 
+    // wall
+    if (player.x+player.radius >= 48 && player.x-player.radius <= 52 && player.y <= 75) {
+      if (player.x < 48) {
+        player.dx = 0;
+        player.x = 48-player.radius;
+      }else {
+        player.dx = 0;
+        player.x = 52+player.radius;
+      }
+    }
+
     await this.setState((state, props) => ({
-      player: player
+      [playerSide]: player
+    }))
+  }
+
+  async ballMove () {
+    const ball = await Object.assign({}, this.state.ball);
+    ball.dy -= ball.gravity;
+    ball.x += ball.dx/2;
+    ball.y += ball.dy;
+
+    if(ball.x < ball.radius*2) {
+      ball.dx *= -1;
+    }
+    if(ball.x > 100-ball.radius*2) {
+      ball.dx *= -1;
+    }
+    if(ball.y < ball.radius*2) {
+      ball.y = ball.radius*2;
+      ball.dy = -ball.dy * ball.drag;
+      ball.dx = ball.dx * ball.drag;
+    }
+    // wall
+    if (ball.x+ball.radius >= 48 && ball.x-ball.radius <= 52 && ball.y <= 75) {
+      if (ball.x < 48) {
+        ball.dx *= -1;
+        ball.x = 48-ball.radius;
+      }else {
+        ball.dx *= -1;
+        ball.x = 52+ball.radius;
+      }
+    }
+
+    await this.setState((state, props) => ({
+      ball: ball
     }))
   }
 
@@ -90,7 +164,7 @@ class Game extends React.Component {
   }
 
   componentDidMount() {
-    const socket = io.connect('http://172.20.1.39:3030');
+    const socket = io.connect('http://172.20.1.20:3030');
 
     socket.on('ball point', (data) => {
       this.setState((state, props) => ({
@@ -98,9 +172,29 @@ class Game extends React.Component {
       }));
     });
 
+    socket.on('enemy point', (data) => {
+      this.setState((state, props) => ({
+        enemy: Object.assign(state.enemy, JSON.parse(data))
+      }));
+    });
+
+    document.querySelector('canvas').addEventListener('mousedown', async (e) => {
+      if (e.button === 0) {
+        await this.setState((state, props) => ({
+          player: Object.assign(state.player, { Kick: true })
+        }));
+      }
+    });
+
+    document.querySelector('canvas').addEventListener('mouseup', async (e) => {
+      if (e.button === 0) {
+        await this.setState((state, props) => ({
+          player: Object.assign(state.player, { Kick: false })
+        }));
+      }
+    });
 
     document.addEventListener('keydown', async (e) => {
-
       switch (e.code) {
         case "KeyW": await this.setState((state, props) => ({
           player: Object.assign(state.player, { Up: true })
@@ -113,11 +207,9 @@ class Game extends React.Component {
         })); break;
         default: break;
       }
-
     });
 
     document.addEventListener('keyup', async (e) => {
-
       switch (e.code.toString()) {
         case "KeyW": await this.setState((state, props) => ({
           player: Object.assign(state.player, { Up: false })
@@ -130,19 +222,69 @@ class Game extends React.Component {
         })); break;
         default: break;
       }
+    });
 
+    document.querySelector('canvas').addEventListener('mousemove', async (e) => {
+      await this.setState((state, props) => ({
+        aim: { x: Math.floor(e.layerX/window.innerWidth*100), y: 100-Math.floor(e.layerY/window.innerHeight*100) }
+      }));
     });
 
     setInterval(async () => {
       await this.fieldClear();
-      await this.playerFigure(this.state.player.x * window.innerWidth / 100, (100 - this.state.player.y) * window.innerHeight / 100, 'green', this.state.player.radius);
+      await this.playerFigure(this.state.player.x * window.innerWidth / 100, (100 - this.state.player.y) * window.innerHeight / 100, this.state.player.color, this.state.player.radius);
+      // await this.playerFigure(this.state.enemy.x * window.innerWidth / 100, (100 - this.state.enemy.y) * window.innerHeight / 100, 'blue', this.state.enemy.radius);
+
       await this.ballFigure(this.state.ball.x * window.innerWidth / 100, (100 - this.state.ball.y) * window.innerHeight / 100, 'gray', this.state.ball.radius);
     }, 0);
 
     setInterval(async () => {
+      const ball = await Object.assign({}, this.state.ball);
+      const player = await Object.assign({}, this.state.player);
+      const aim = await Object.assign({}, this.state.aim);
+
+      let Dx = ball.x - player.x;
+      let Dy = ball.y - player.y;
+      let d = Math.sqrt(Dx*Dx+Dy*Dy);
+
+      let rb = ball.radius;
+      let rp = player.radius;
+
+      if (d < rb + rp && player.Kick) {
+        const hx = aim.x - player.x;
+        const hy = aim.y - player.y;
+        const r = Math.sqrt(hx*hx+hy*hy);
+
+        const ax = hx/r;
+        const ay = hy/r;
+
+
+
+        if(aim.x > player.x)
+          ball.dx = ax;
+        else
+          ball.dx = -ax;
+
+        if(aim.y > player.y)
+          ball.dy = ay;
+        else
+          ball.dy = -ay;
+
+
+        await socket.emit('ball point', JSON.stringify(this.state.ball));
+      }
+
+      await this.setState((state, props) => ({
+        ball: ball
+      }));
+
       await this.playerMove();
-      socket.emit('player point', JSON.stringify(this.state.player));
-    }, 100);
+      await this.ballMove();
+    }, 1);
+
+    // setInterval(async () => {
+    //   await socket.emit('enemy point', JSON.stringify(this.state.player));
+    // }, 100);
 
   }
 
